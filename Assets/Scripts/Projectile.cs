deba16a6344edcea846393a71bfefa5b2d1eeb73
TheMagicalFishTaco <50703929+TheMagicalFishTaco@users.noreply.github.com>
2021-11-07 13:49:46 +0800

    New enemy added to the enemy list. Collisions fixed again

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -11,9 +11,15 @@ public class Projectile : MonoBehaviour
     //tells the projectile to ignore collisions with the player
     private void Start()
     {
+        //tells enemy projectiles to ignore colliding with the enemy spawn border
         enemySpawn = GameObject.FindGameObjectsWithTag("EnemySpawn");
+        if (gameObject.tag == "EnemyProjectile")
+        {
+            Physics2D.IgnoreCollision(enemySpawn[0].GetComponent<Collider2D>(), GetComponent<Collider2D>());            
+        }
+
+        //tells all projectiles to ignore colliding with their parent (enemy for enemy projectile, player for player projectile)
         Physics2D.IgnoreCollision(transform.parent.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-        Physics2D.IgnoreCollision(enemySpawn[0].GetComponent<Collider2D>(), GetComponent<Collider2D>());
 
         //The following code finds gameobjects tagges as player projectiles and enemyprojectiles, and tells the current projectile
         //to ignore collisions with them.
@@ -34,17 +40,26 @@ public class Projectile : MonoBehaviour
         }
     }
 
-    //This method makes sure that the projectile is destroyed whenever it collides with anything
-    //So that would be the borders, the player, and the enemy
-    
+    //this method handles collisions and instantiates the explosion effect
+    //split into player projectile and enemy projectile for ease of use
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("EnemySpawn") || col.gameObject.CompareTag("Ground") || col.gameObject.CompareTag("Enemy") || col.gameObject.CompareTag("Player"))
+        if (gameObject.tag == "PlayerProjectile")
         {
-            GameObject projectileExplosion = Instantiate(explosionEffect, gameObject.transform.position, gameObject.transform.rotation);
-            Destroy(gameObject);
-
-
+            if (col.gameObject.CompareTag("EnemySpawn") || col.gameObject.CompareTag("Ground") || col.gameObject.CompareTag("Enemy"))
+            {
+                GameObject projectileExplosion = Instantiate(explosionEffect, gameObject.transform.position, gameObject.transform.rotation);
+                Destroy(gameObject);
+            }
         }
+        else if (gameObject.tag == "EnemyProjectile")
+        {
+            if (col.gameObject.CompareTag("Ground") || col.gameObject.CompareTag("Player"))
+            {
+                GameObject projectileExplosion = Instantiate(explosionEffect, gameObject.transform.position, gameObject.transform.rotation);
+                Destroy(gameObject);
+            }
+        }
+
     }
 }
