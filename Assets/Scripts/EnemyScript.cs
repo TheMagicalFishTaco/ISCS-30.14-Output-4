f412972d5f610e00dfa05393d6d7c992d358f5f9
TheMagicalFishTaco <50703929+TheMagicalFishTaco@users.noreply.github.com>
2021-11-07 13:24:52 +0800

    github wtf

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -56,7 +56,6 @@ public class EnemyScript : MonoBehaviour
         //I think the explosion stuffs can be put here
         //either instantiate an explosion sprite then destroy it after 1s or so
         //or start an animation for the explosion
-
         if (col.gameObject.CompareTag("Ground") || col.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
